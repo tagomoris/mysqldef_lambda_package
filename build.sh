b7ace750c497309df223f573508f9dc5bfbf63dc
Satoshi Moris Tagomori <tagomoris@gmail.com>
2023-05-02 12:07:07 +0900

    fix the container name

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ mkdir -p tmp
     curl -L -O https://github.com/k0kubun/sqldef/releases/download/v0.15.22/mysqldef_linux_amd64.tar.gz
     tar xzf mysqldef_linux_amd64.tar.gz
 )
-docker build -t pt-mysqldef .
+docker build -t mysqldef-lambda .
 rm -rf tmp
